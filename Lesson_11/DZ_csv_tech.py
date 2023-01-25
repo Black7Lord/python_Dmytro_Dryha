b78b319e137a0c8934458f7c24b639fa49a4b493
shkip <shkiper13579017@gmail.com>
2023-01-25 19:26:15 +0200

    DZ_csv_tech

--- Lesson_11/DZ_csv_tech.py
+++ Lesson_11/DZ_csv_tech.py
@@ -1,84 +1,122 @@
-import json
 import csv
 from uuid import uuid4
 
-def create_index(employee_uids: dict, index_key: str) -> dict:
+def create_index(tech_uids: dict, index_key: str) -> dict:
+    """
+    Функція створює індекс по категоріям/брендам.
+    :param tech_uids: індекс унікальних айді для кожного запису
+    :param index_key: рядок-назва категорії/бренду
+    :return: new_index: створений індекс по категоріям/брендам
+    """
     new_index = dict()
-    for uid, employee in employee_uids.items():
-        if employee[index_key] in new_index:
-            new_index[employee[index_key]].append(uid)
+    for uid, tech in tech_uids.items():
+        if tech[index_key] in new_index:
+            new_index[tech[index_key]].append(uid)
         else:
-            new_index[employee[index_key]] = [uid]
+            new_index[tech[index_key]] = [uid]
     return new_index
 
-def positions_in_department_view(
-        employee_uids: dict, _department_index: dict,
-        _department_name: str, debug: bool = False
+def brands_in_category_view(
+        tech_uids: dict, _category_index: dict,
+        _category_name: str, debug: bool = False
 ) -> dict:
     """
-    Функція рахує кількість посад у вказаному відділі.
-    У словнику, що повертається, будуть лише ті посади, які є у відділу, а не всі.
-    :param _department_name: ім'я відділу
-    :param _department_index: індекс відділу
-    :param employee_uids: індекс співробітників
+    Функція рахує кількість брендів у вказаній категорії.
+    У словнику, що повертається, будуть лише ті бренди, які є у категорії, а не всі.
+    :param _category_name: ім'я категорії
+    :param _category_index: індекс категорії
+    :param tech_uids: індекс унікальних айді для кожного запису
     :param debug: прапорець для перевірки коректної роботи
-    :return: словник: ключ - посади у відділі, значення - їхня кількість
+    :return: brands_in_category: словник, де ключ - бренди у категорії, а значення - їхня кількість
     """
-    #print(f'У відділі {_department_name} працюють:')
-    positions_in_department = dict()
-    for _uid in _department_index[_department_name]:
+    brands_in_category = dict()
+    for _uid in _category_index[_category_name]:
         if debug:
-            print(employee_uids[_uid])
-        employee_position = employee_uids[_uid]['position']
-        if employee_position in positions_in_department:
-            positions_in_department[employee_position] += 1
+            print(tech_uids[_uid])
+        tech_brand = tech_uids[_uid]['brand']
+        if tech_brand in brands_in_category:
+            brands_in_category[tech_brand] += 1
         else:
-            positions_in_department[employee_position] = 1
-    # print(positions_in_department)
-    return positions_in_department
+            brands_in_category[tech_brand] = 1
+    return brands_in_category
 
 if __name__ == '__main__':
-
     data = {
         'data': list()
     }
-    with open('hr_department.csv', newline='') as csvfile:
+    with open('tech_inventory.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             data['data'].append(row)
+
+    # виведення словника із усіма записами для перевірки (не є частиною ДЗ)
     print('1. ', type(data), data)
 
-    employee_ids_index = dict()
-    for employee in data['data']:
-        _id = f'{employee["name"]}{employee["surname"]}{employee["surname"]}{employee["position"]}{employee["department"]}{employee["salary"]}'
+    # створення індексу унікальних айді для кожного запису
+    tech_ids_index = dict()
+    for item in data['data']:
         _uid = uuid4()
-        employee_ids_index[_uid] = employee
+        tech_ids_index[_uid] = item
 
-    """
-            "name": "Will",
-            "surname": "Collier",
-            "position": "Manager",
-            "department": "Production",
-            "salary": 2400
-    """
+    # створення індексу по брендам та виведення кількості товарів кожного бренду
+    brand_index = create_index(tech_ids_index, 'brand')
+    for key, value in brand_index.items():
+        print(f'2. Від бренду "{key}" є {len(value)} товарів')
 
-    position_index = create_index(employee_ids_index, 'position')
-    for key, value in position_index.items():
-        print(f'2. На посаді {key} працює {len(value)} людей.')
+    # створення індексу по категоріям та виведення кількості товарів в кожній категорії
+    category_index = create_index(tech_ids_index, 'category')
+    for key, value in category_index.items():
+        print(f'3. Серед категорії "{key}" є {len(value)} товарів')
 
-    department_index = create_index(employee_ids_index, 'department')
-    for key, value in department_index.items():
-        print(f'3. У відділі {key} працює {len(value)} людей.')
+    # виведення на екран переліку повної інформації про кожний товар одного обраного бренда
+    # реалізація вибору бренду шляхом введення числа напроти назви у виведеному списку
+    print('Виберіть бренд, товари якого ви хочете побачити:')
+    i = 1
+    brand = 'Logitech'
+    category_list = list(brand_index.keys())
 
-    print('4. У відділі HR працюють:')
-    for uid in department_index['HR']:
-        print(employee_ids_index[uid])
+    for b in category_list:
+        print(f'{i}. {b}')
+        i += 1
+    choice_brand = input(">>>")
+    try:
+        int_choice_brand = int(choice_brand)
+        if int_choice_brand > 0 and int_choice_brand <= len(category_list):
+            brand = category_list[int_choice_brand - 1]
+        else:
+            print(f'Невірне значення. Буде виведено товари бренду "{brand}".')
+    except Exception:
+        print(f'Невірне значення. Буде виведено товари бренду "{brand}".')
 
-# знайти скільки <посада> у <відділ>
-# 1) користуватися тільки одним індексом, потім фільтрувати кожну групу
-# 2) set + операція intersection
+    print(f'4. Бренд "{brand}":')
+    for uid in brand_index[brand]:
+        print(tech_ids_index[uid])
 
-    for department_name in department_index.keys():
-        print(f'У відділі {department_name} працюють такі посади: ', positions_in_department_view(employee_ids_index, department_index, department_name))
+    # виведення на екран переліку повної інформації про кожний товар однієї обраної категорії
+    # реалізація вибору категорії шляхом введення числа напроти назви у виведеному списку
+    print('Виберіть категорію, з якої ви хочете побачити товари:')
+    i = 1
+    category = 'Smartphones'
+    category_list = list(category_index.keys())
 
-    # print(positions_in_department_view(employee_ids_index, department_index, 'Art'))
+    for c in category_list:
+        print(f'{i}. {c}')
+        i += 1
+    choice_cat = input(">>>")
+    try:
+        int_choice_cat = int(choice_cat)
+        if int_choice_cat > 0 and int_choice_cat <= len(category_list):
+            category = category_list[int_choice_cat - 1]
+        else:
+            print(f'Невірне значення. Буде виведено товари з категорії "{category}".')
+    except Exception:
+        print(f'Невірне значення. Буде виведено товари з категорії "{category}".')
+
+    print(f'5. Категорія "{category}":')
+    for uid in category_index[category]:
+        print(tech_ids_index[uid])
+
+    # виведення на екран розподілу товарів по брендам для кожної категорії
+    for category_name in category_index.keys():
+        print(f'6. У категорії "{category_name}" представлені товари таких брендів: ',
+              brands_in_category_view(tech_ids_index, category_index, category_name))
